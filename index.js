1f4a980ec62602652b19b67b4b7366e4d6b6eff6
Derek Smyth <smyth.derek@gmail.com>
2023-01-17 23:29:17 -0500

    commit tree 53ac6b79d33d9ee1c01dac716451e30e2d1164ce

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+const bdayCard = [];
+
+function writeCards(names, event) {
+    for (let i = 0; i < names.length; i++) {
+    bdayCard.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    };
+    return bdayCard;
+};
+
+function countDown(int) {
+    while (int >= 0) {
+        console.log(int);
+        int--;
+    }
+}
