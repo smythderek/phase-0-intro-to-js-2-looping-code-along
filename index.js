97d5727fe5676afd4e9f5cf1072c623b4f850e2b
Derek Smyth <smyth.derek@gmail.com>
2023-02-28 20:00:04 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+
+function writeCards(array, event) {
+    const newArray = [];
+    for (let i = 0; i < array.length; i++) {
+        newArray.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`)
+    }
+    
+    return newArray;
+}
+
+writeCards(["Charlie", "Samip", "Ali"], "birthday");
+
+function countDown(integer) {
+    while (integer >= 0) {
+        console.log(integer);
+        integer--;
+    }
+}
+
+countDown(5);
